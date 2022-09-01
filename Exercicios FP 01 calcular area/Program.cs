a4b8f67372607c2b6ad92db734a1f42fb4a4f2aa
Diego Mamede <111023189+diegomamede1@users.noreply.github.com>
2022-09-01 00:12:35 -0300

    Add files via upload

---
+++ Exercicios FP 01 calcular area/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace aula1;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          double raio;
+          double area;
+          Console.Write("Raio: ");
+          raio = Convert.ToDouble(Console.ReadLine ());
+          area = 3.1415 * raio * raio;
+          Console.Write("Area: ");
+          Console.WriteLine(area);
+        }
+    }
+
