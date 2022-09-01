d43b30c4a5ef87a7555ea38dc0529f8a185dfe7f
Diego Mamede <111023189+diegomamede1@users.noreply.github.com>
2022-09-01 00:22:17 -0300

    Add files via upload

---
+++ Exercicios FP 08 Matriz/Exercicio n3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Exercicios_Matriz_FP_08
+{
+  class Program
+  {
+    static void Soma(int[,] matriz1, int[,] matriz2, int[,] soma)
+    {
+
+      int i, j;
+      for (i = 0; i < 3; i++)
+      {
+
+        for (j = 0; j < 3; j++)
+        {
+
+          soma[i, j] = matriz1[i, j] + matriz2[i, j];
+          Console.WriteLine("Soma das matrizes: {0}", soma[i, j]);
+
+        }
+
+      }
+
+    }
+    static void LeMatriz(int[,] matriz1, int[,] matriz2)
+    {
+      int i, j;
+      for (i = 0; i < 3; i++)
+      {
+
+        for (j = 0; j < 3; j++)
+        {
+          Console.WriteLine("informe o valor da MATRIZ1 linha {0} e coluna {1}:", i, j);
+          matriz1[i, j] = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+      }
+
+      for (i = 0; i < 3; i++)
+      {
+
+        for (j = 0; j < 3; j++)
+        {
+
+          Console.WriteLine("informe o valor da MATRIZ2 linha {0} e coluna {1}:", i, j);
+          matriz2[i, j] = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+      }
+
+
+    }
+    static void Main(string[] args)
+    {
+
+      int[,] matriz1 = new int[3, 3];
+      int[,] matriz2 = new int[3, 3];
+      int[,] soma = new int[3, 3];
+
+      LeMatriz(matriz1, matriz2);
+      Soma(matriz1, matriz2, soma);
+
+
+    }
+  }
+}
---
+++ Exercicios FP 08 Matriz/Exercicio n4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Nova_pasta
+{
+    class Program
+    {
+        static void invertido ( double[,] numeros)
+        {
+            double[,] inversao = new double [3,3];
+            for(int i=0; i<3;i++)
+            {
+                for(int j=0; j<3; j++)
+                {
+                    inversao[j,i] = numeros[i,j];
+                    
+                }
+            }
+            for (int i=0; i<3;i++)
+            {
+                for(int j=0; j<3; j++)
+                {
+                    Console.Write(" {0} ",inversao[i,j]);
+                }
+                Console.WriteLine();
+            }
+               
+        }
+        static void Main(string[] args)
+        {
+            double[,] numeros = new double [3,3];
+            for (int i=0; i<3; i++)
+            {
+                for( int j=0; j<3; j++)
+                {
+                    Console.WriteLine("insira linha {0} e coluna {1}",i+1,j+1);
+                    numeros[i,j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            invertido(numeros);
+        }
+    }
+}
